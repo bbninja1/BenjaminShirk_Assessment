29ee619265453cb02640c7e1d89e24374b619aa2
Benjamin Shirk <shirk.benjamin@gmail.com>
2024-01-31 20:04:45 +0200

    Add project files.

---
+++ RabbitMQ_Library/Connection.cs
@@ -0,0 +1,48 @@
+﻿using System.Text;
+using System.Xml.Linq;
+using RabbitMQ.Client;
+using Newtonsoft.Json;
+
+namespace RabbitMQ_Library
+{
+    public class Connection
+    {
+        /// <summary>
+        /// Connection Method to the RabbitMQ server
+        /// </summary>
+        /// <param name="name"></param>
+        public static void RabbitMQConnection(string name)
+        {
+            try
+            {
+                var Conn = new ConnectionFactory
+                {
+                    //default settings
+                    Uri = new Uri("amqp://guest:guest@localhost:5672")
+                };
+
+                //Create the connection and model
+                using var connection = Conn.CreateConnection();
+                using var channel = connection.CreateModel();
+
+                channel.QueueDeclare("message-queue", durable: true, exclusive: false, autoDelete: false, arguments: null);
+
+                //sets the message to the required format
+                string Message = "Hello my name is, " + name;
+
+                var body = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(Message));
+
+                //publishes the message to the queue
+                channel.BasicPublish("", "message-queue", null, body);
+                //confirmation of the message sent to the queue
+                Console.WriteLine("The Message: " + Message + ". has been sent to the queue");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error occurred: {ex.Message}");
+            }
+        }
+    }
+
+}
+//-------------------------------------End OF File------------------------------------------//
---
+++ WorkerClassLibrary_Test/WorkerClassTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ_Sender;
+using RabbitMQ_Library;
+using Xunit;
+
+namespace WorkerClassLibrary_Test
+{
+    public class WorkerClassTests
+    {
+        //Test for Name Validation
+        [Theory]
+        [InlineData("John", true)]            
+        [InlineData("123", false)]            
+        [InlineData("John Doe", false)]       
+        [InlineData("", false)]               
+        [InlineData(null, false)]             
+        public void IsNameValid_Validations(string input, bool expectedResult)
+        {
+            //Arrange
+            WorkerClass worker = new WorkerClass();
+
+            //Act
+            bool result = worker.isNameValid(input);
+
+            //Assert
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
